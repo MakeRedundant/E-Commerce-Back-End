31418451dc90e32c5373eeaf812b4e0c2998c11e
MakeRedundant <105352286+MakeRedundant@users.noreply.github.com>
2023-08-16 20:15:56 +1000

    defined endpoints for find all and find one

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -1,27 +1,64 @@
-const router = require('express').Router();
-const { Category, Product } = require('../../models');
-
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
 // The `/api/categories` endpoint
+//Defining an API endpoint
 
-router.get('/', (req, res) => {
+//Gets all the cateogries with their associated products
+router.get("/", (req, res) => {
   // find all categories
-  // be sure to include its associated Products
+  Category.findAll({
+    attributes: ["id", "category_name"],
+    include: [
+      {
+        model: Product,
+        attributes: ["id", "product_name", "price", "stock", "category_id"],
+      },
+    ],
+  })
+    .then((dbCategoryData) => res.json(dbCategoryData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err); //means something went wrong on the server end and it coudnt handle the request properly.
+    });
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "category_name"],
+    // be sure to include its associated Products
+    include: [
+      {
+        model: Product,
+        attributes: ["id", "product_name", "price", "stock", "category_id"],
+      },
+    ],
+  })
+    .then((dbCategoryData) => {
+      if (!dbCategoryData) {
+        res.status(404).json({ message: "No category found with this id" });
+        return;
+      }
+      res.json(dbCategoryData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // create a new category
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // delete a category by its `id` value
 });
 
