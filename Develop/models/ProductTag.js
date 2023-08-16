c3d7491c96325863e55b93a73978995a7faef503
MakeRedundant <105352286+MakeRedundant@users.noreply.github.com>
2023-08-16 22:31:00 +1000

    defined product tags model

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -1,19 +1,41 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        //the reference tag is used to establish foreign key relationships
+        model: "product", // The referenced model is 'product'
+        key: "id", // The referenced column in the 'product' model is 'id',
+      }, //between ProductTag and the Product Table
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "tag", // The referenced model is 'tag'
+        key: "id", // The referenced column in the 'tag' model is 'id'
+      },
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product_tag',
+    modelName: "product_tag",
   }
 );
 
