f50b94af9d5ee8ca3702804be4e14ba495a29df3
MakeRedundant <105352286+MakeRedundant@users.noreply.github.com>
2023-08-19 22:37:00 +1000

    updated category-routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -43,7 +43,9 @@ router.get("/:id", (req, res) => {
     .then((categoryData) => {
       if (!categoryData) {
         //if no category is found than return 404
-        res.status(404).json({ message: "No category found with this id" });
+        res
+          .status(404)
+          .json({ message: "Unsuccessful, No category found with this id" });
         return;
       }
       res.json(categoryData); //if a category is found than retrieve the data in JSON format
@@ -89,12 +91,15 @@ router.put("/:id", (req, res) => {
     });
 });
 
+//Delete Category
 router.delete("/:id", (req, res) => {
+  //DELETE HTTP method is defined and  ':id specifies which category to delete based on unique ID,
   Category.destroy({
+    //Category,destory method is called to delete a category and takes a object from the where property based on the id from req,params.id
     where: {
       id: req.params.id,
     },
-  })
+  }) ////Promise handling with then method is used to handle the promise to return the category.destroy
     .then((deleteCategory) => {
       if (!deleteCategory) {
         res.status(404).json({
@@ -102,7 +107,7 @@ router.delete("/:id", (req, res) => {
         });
         return;
       }
-      res.json(deleteCategory);
+      res.json(deleteCategory); //if a error occurs during the deletion process/ something unexpected happened
     })
     .catch((err) => {
       console.log(err);
@@ -118,3 +123,23 @@ module.exports = router;
 // PUT: "Update this existing data." Used to modify data that's already on the server. Like editing a text document and saving the changes.
 
 // DELETE: "Remove this piece of data." Used to ask the server to remove something
+
+// HTTP status codes
+// Informational Codes (1xx):
+// 100: Continue - The server has received the initial part of the request and is willing to proceed.
+// 101: Switching Protocols - The server agrees to switch protocols as requested by the client.
+// Successful Codes (2xx):
+// 200: OK - The request was successful, and the server has returned the requested resource.
+// 201: Created - The request was successful, and a new resource was created as a result.
+// 204: No Content - The server successfully processed the request but does not need to return any content.
+// Redirection Codes (3xx):
+// 301: Moved Permanently - The requested resource has been permanently moved to a new location.
+// 302: Found - The requested resource has been temporarily moved to a different location.
+// 304: Not Modified - The client's cached version of the requested resource is still valid.
+// Client Error Codes (4xx):
+// 400: Bad Request - The server could not understand the request due to malformed syntax or invalid parameters.
+// 401: Unauthorized - The client must authenticate itself to get the requested resource.
+// 404: Not Found - The requested resource could not be found on the server.
+// Server Error Codes (5xx):
+// 500: Internal Server Error - The server encountered an unexpected condition that prevented it from fulfilling the request.
+// 503: Service Unavailable - The server is temporarily unable to handle the request due to maintenance or overload.
